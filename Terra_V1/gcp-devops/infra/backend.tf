fd34147f4e4d3bdab5f25e125080f42753a89519
Amit <amit@email.com>
2026-02-23 01:35:03 +0530

    19st

--- Terra_V1/gcp-devops/infra/backend.tf
+++ Terra_V1/gcp-devops/infra/backend.tf
@@ -1,7 +1,7 @@
 #backend...................
 terraform {
     backend "gcs" {
-        bucket = "terraform_state_prod_v1"
+        bucket = "terraform-state-prod_v1"
         prefix = "gke/infra"
     }   
 }
